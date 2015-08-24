c48fc562b1c77bff485bb868a3de0338567a8a17
Buddha Prakash <bp@Buddhas-MacBook-Pro.local>
2015-08-24 22:32:31 +0530

    Fix bug in state search expansion

--- solver.cpp
+++ solver.cpp
@@ -1,22 +1,53 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <set>
+#include <utility>
 #define MOD 1298307964911120441ULL
+typedef unsigned long long int lli; 
 using namespace std;
 
+struct compare  
+{  
+   bool operator()(const pair<int, lli> & l, const pair<int,lli>& r)  
+   {  
+       return l.first > r.first;
+   }  
+};
+
+// Global variables to store the path and hashes
 map<lli, vector<vector<int> > > hashStore;
 map<lli, lli> parent;
 map<lli, int> dist; 
-map<lli, pair<pair<int,int > > > parentJump;
+map<lli, pair<pair<int,int> ,pair<int,int> > > parentJump;
+priority_queue<pair<int, lli> ,vector<pair<int, lli> > , compare > nextMoves;
+
 int numColors, dimMatrix;
 
 int dirx[4] = {0,1,0,-1};
 int diry[4] = {1,0,-1,0};
+
 
 bool validMove(int x,int y)
 {
 	if(x>=0 and y>=0 and x<dimMatrix and y<dimMatrix)
 		return true;
 	return false;
+}
+void printMat(vector<vector<int> > &mat)
+{
+	for(int i=0;i<dimMatrix;i++)
+	{
+		for(int j=0;j<dimMatrix;j++)
+		{
+			cout<<mat[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
 }
 
 void getInitialConfiguration(vector<vector<int> > &initialConfiguration)
@@ -30,7 +61,7 @@ void getInitialConfiguration(vector<vector<int> > &initialConfiguration)
 			cin>>tempVal;
 			tempVec.push_back(tempVal);
 		}
-		initialConfiguration.push_back(tempVec)
+		initialConfiguration.push_back(tempVec);
 	}
 }
 
@@ -58,12 +89,12 @@ bool checkSolutionFound(vector<vector<int> > &mat)
 				counter+=1;
 		}
 	}
-	if(counter==1)
+	if(counter == 1)
 		return true;
 	return false;
 }
 
-int getHash(vector<vector<int> > &mat)
+lli getHash(vector<vector<int> > &mat)
 {
 	lli base = numColors;
 	lli hashVal = 0 ;
@@ -73,7 +104,7 @@ int getHash(vector<vector<int> > &mat)
 		for(int j=0;j<dimMatrix;j++)
 		{
 			powBase = (powBase * base)%MOD;
-			lli val = (powBase * mat[i][j]) % MOD;
+			lli val = (powBase * (lli)mat[i][j]) % MOD;
 			hashVal = (hashVal + val)%MOD;
 		}
 	}
@@ -81,13 +112,15 @@ int getHash(vector<vector<int> > &mat)
 	return hashVal;
 }
 
-lli addMoves(vector<vector<int> > &config, priority_queue<pair<int, lli> > nextMoves)
+lli addMoves(vector<vector<int> > &config)
 {
 
 	for(int i=0;i<dimMatrix;i++)
 	{
 		for(int j=0;j<dimMatrix;j++)
 		{
+			if(config[i][j] == 0)
+				continue ;
 			for(int k=0;k<4;k++)
 			{
 				int valid = false;
@@ -118,14 +151,14 @@ lli addMoves(vector<vector<int> > &config, priority_queue<pair<int, lli> > nextM
 						currX += dirx[k];
 						currY += diry[k];
 					}
-
+					newConfig[currX][currY] = config[i][j]; 
 					lli newHashVal = getHash(newConfig);
 					lli origHashVal = getHash(config);
 					parentJump[newHashVal] = make_pair(make_pair(i+1,j+1),make_pair(currX+1,currY+1));
 					dist[newHashVal] = dist[origHashVal] + 1;
 					parent[newHashVal] = origHashVal;
 					lli distinctColors = getNumberOfDistinctColors(newConfig) - 1;
-					nextMoves.push(make_pair(distinctColors + dist[newHashVal]));
+					nextMoves.push(make_pair(distinctColors + dist[newHashVal], newHashVal));
 					if(checkSolutionFound(newConfig))
 						return newHashVal;
 				}
@@ -134,9 +167,12 @@ lli addMoves(vector<vector<int> > &config, priority_queue<pair<int, lli> > nextM
 	}
 	return -1;
 }
+
+
 void printSolution(lli finalState)
 {
-	vector<pair<pair<int,int> > > ans;
+	char te;
+	vector<pair<pair<int,int> ,pair<int,int> > > ans;
 	while(parent[finalState] != -1)
 	{
 		ans.push_back(parentJump[finalState]);
@@ -151,21 +187,20 @@ void printSolution(lli finalState)
 
 int main()
 {
-	freopen("input.txt","r",stdin);
+	// freopen("input.txt","r",stdin);
 	vector<vector<int> > initialConfiguration;
-	priority_queue<pair<int, lli>, greater<pair<int, lli> > > nextMoves;
 	lli finalState;
 	getInitialConfiguration(initialConfiguration);
 	lli hashVal = getHash(initialConfiguration);
 	parent[hashVal] = -1;
 	dist[hashVal] = 0;
 	int distinctColors = getNumberOfDistinctColors(initialConfiguration) - 1;
-	nextMoves.push(mp(distinctColors, hashVal));
+	nextMoves.push(make_pair(distinctColors, hashVal));
 	while(!nextMoves.empty())
 	{
 		pair<int,lli> topElement = nextMoves.top();
 		nextMoves.pop();
-		finalState = addMoves(hashStore[topElement.second], nextMoves);
+		finalState = addMoves(hashStore[topElement.second]);
 		if(finalState != -1)
 			break;
 	}
